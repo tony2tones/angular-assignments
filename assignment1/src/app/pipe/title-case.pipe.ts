6f7f502e705371591c23af20c0535513fd9c77c0
Anthony <anthonymarques@outlook.com>
2019-04-03 12:11:02 +0200

    refactored pipe case

--- assignment1/src/app/pipe/title-case.pipe.ts
+++ assignment1/src/app/pipe/title-case.pipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from "@angular/core";
+import { prepareSyntheticListenerFunctionName } from "@angular/compiler/src/render3/util";
 
 @Pipe({
   name: "titleCase"
@@ -7,18 +8,22 @@ export class TitleCasePipe implements PipeTransform {
   transform(value: string): any {
     if (!value) return null;
 
-    let prepersitions = [
-      'of',
-      'a'
-    ];
-
-    let words = value.split(' ');
-    for(var i = 0; i < words.length; i++){
-      if(prepersitions.includes(words[i])){
-        words[i].toLocaleLowerCase();
-      }
-      else { words[i].substring(0,1).toUpperCase() + words[i].substring(1).toLocaleLowerCase(); 
+    let words = value.split(" ");
+    for (var i = 0; i < words.length; i++) {
+      if (i > 0 && this.isPrepersition(words[i])) {
+        words[i] = words[i].toLowerCase();
+      } else {
+        words[i] = this.formatWords(words[i]);
       }
     }
+    return words.join(" ");
+  }
+  private isPrepersition(word: string): boolean {
+    let prepersitions = ["of", "the"];
+    return prepersitions.includes(word.toLowerCase());
+  }
+
+  private formatWords(word:string) {
+    return (word.substr(0,1).toUpperCase() + word.substr(1).toLowerCase());
   }
 }
